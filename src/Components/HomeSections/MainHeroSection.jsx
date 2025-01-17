d9ff3d71361f82f727c854fa071220400594db04
Drivenstudiosweb <steve@drivenstudios.com>
2025-01-16 16:33:04 -0800

    beta 1

--- src/Components/HomeSections/MainHeroSection.jsx
+++ src/Components/HomeSections/MainHeroSection.jsx
@@ -177,14 +177,14 @@ const MainHeroSection = () => {
                 <div id='text-content-1' className="bg-[#E72427] flex justify-center items-center  py-5 md:py-[0px] md:px-[0px]">
                     {/* Heading */}
                     <div id='heading-1' className="text-white text-5xl w-[70%] overflow-hidden">
-                    <div class="crop-wrapper">
+                   {/* <div class="crop-wrapper">
                         <div class="cropper-text">
                             
                            <img src={header1} alt="" />
                         
                             </div>
                             </div>
-                        {/*}
+                        */}
                         <span id='' className="w-[80%] ">
                             <div className="HeroSecHeader">
                                 Where the
@@ -212,7 +212,7 @@ const MainHeroSection = () => {
                                 <h1 className='text-white'>faster things meet</h1>
                             </div>
                         </span>
-                        */}
+                        
                     </div>
 
                     {/* Text and Button Section */}
