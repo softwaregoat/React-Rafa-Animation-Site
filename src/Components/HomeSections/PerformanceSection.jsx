7ac15ab05118073f826896b3c4c6d5ea679eaa56
Drivenstudiosweb <steve@drivenstudios.com>
2025-01-17 14:47:09 -0800

    Update PerformanceSection.jsx

--- src/Components/HomeSections/PerformanceSection.jsx
+++ src/Components/HomeSections/PerformanceSection.jsx
@@ -139,7 +139,7 @@ const PerformanceSection = () => {
         <>
             <div className="bg-[#0c0c0c] min-h-screen md:px-12 pb-20  py-16" >
                 <div className='block md:hidden uppercase text-[#ba2427] text-8xl overflow-hidden opacity-[90%] text-center pb-[1rem]'>
-                    <h1 className='text-[#BA2427] leading-[80px] [@media(max-width:390px)]:text-[5rem] leading-[3rem] max-[380px]:text-[2.8rem] max-[380px]:leading[2.4rem]'>Perfor<br/>mance<br/>center</h1>
+                    <h1 className='text-[#BA2427] leading-[80px]  leading-[3rem] max-[380px]:text-[2.8rem] max-[380px]:leading[2.4rem]'>Perfor<br/>mance<br/>center</h1>
 
                 </div>
 
